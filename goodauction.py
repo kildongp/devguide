1c226d5db636fb740d9c15458a6db1b3dfb0529b
kildongp <45186657+kildongp@users.noreply.github.com>
2019-04-22 20:48:31 +0900

    Create goodauction.py

---
+++ goodauction.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*- 
+# goodauction.py
+from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
+import json
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+driver = webdriver.Chrome( BASE_DIR + '/chromedriver')
+driver.implicitly_wait(3)
+# url에 접근한다.
+driver.get('https://google.com')
